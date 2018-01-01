7a2c5cad2876f3de67aba5688afe59679c8ebb7c
isao_e <isao0214@gmail.com>
2018-01-02 02:58:29 +0900

    views/home/index.blade.phpを修正

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,12 +1,18 @@
 <!doctype html>
-<html lang="{{ app()->getLocale() }}">
+<html>
     <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>{{ $title = 'title' /* ToDo: titleを動的に表示する */ }}</title>
+        <meta charset="UTF-8">
+        <title>Hello Vue</title>
+        <link rel="stylesheet" href="css/app.css"/>
+        <script type="text/javascript">
+            window.Laravel = window.Laravel || {};
+            window.Laravel.csrfToken = "{{csrf_token()}}";
+        </script>
     </head>
     <body>
-        <p>home</p>
+        <div id="app">
+            @{{ message }}
+        </div>
+        <script src="js/app.js"></script>
     </body>
 </html>
