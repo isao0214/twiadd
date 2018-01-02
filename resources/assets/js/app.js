707c91d3fbf4bb39e1a1d42293afb852e0769af9
isao_e <isao0214@gmail.com>
2018-01-02 22:10:06 +0900

    vue.jsテスト5

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -47,3 +47,15 @@ const app4 = new Vue({
         ]
     }
 })
+
+const app5 = new Vue({
+    el: '#app-5',
+    data: {
+        message: 'Hello Vue.js!'
+    },
+    methods: {
+        reverseMessage: function () {
+            this.message = this.message.split('').reverse().join('')
+        }
+    }
+})
