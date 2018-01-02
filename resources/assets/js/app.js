b046960a5f7803eb5042ea6fa7098c3650372233
isao_e <isao0214@gmail.com>
2018-01-02 23:33:32 +0900

    app.jsでrouterをimport

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 
+import router from './router'
+
+// require('bootstrap-sass')
 require('./bootstrap');
 
 const app = new Vue({
+    router,
     el: '#app',
     render: h => h(require('./app.vue')),
 })
