ad8cdc54e6969d360d3ef916c0987866ccfa3e49
isao_e <isao0214@gmail.com>
2018-01-03 14:18:07 +0900

    Controllers/TaskController.phpの修正

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,9 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Task;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-    //
+    public function index()
+    {
+        return Task::take(5)->get()->keyBy('id');
+    }
+
+    public function store(Request $request)
+    {
+        return Task::create($request->only('name'))->save()->fresh();
+    }
+
+    public function destroy($id)
+    {
+        return Task::destroy($id);
+    }
+
+    public function update($id, Request $request)
+    {
+        return Task::find($id)->fill($request->only('is_done'))
+            ->save()->fresh();
+    }
 }
