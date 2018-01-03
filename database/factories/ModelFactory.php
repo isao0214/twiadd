f1ec6be1475c116f2734554635dab10bec45cf64
isao_e <isao0214@gmail.com>
2018-01-03 20:04:42 +0900

    ModelFactory.phpにUserを追加

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -6,3 +6,12 @@ $factory->define(App\Task::class, function (Faker\Generator $faker) {
         'is_done' => mt_rand(0, 1),
     ];
 });
+
+$factory->define(App\User::class, function (Faker\Generator $faker) {
+    return [
+        'name' => $faker->name,
+        'email' => $faker->unique()->safeEmail,
+        'password' => bcrypt('secret'),
+        'remember_token' => str_random(10),
+    ];
+});
