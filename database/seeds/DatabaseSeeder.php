7dde377a2b5dd8ffff1296968f8377f65dfd86b3
isao_e <isao0214@gmail.com>
2018-01-03 20:06:53 +0900

    DatabaseSeeder.phpを修正

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -4,13 +4,12 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
     public function run()
     {
-        factory(App\Task::class, 50)->create();
+        factory(App\User::class, 50)->create()->each(function ($user) {
+            $user->tasks()->save(
+                factory(App\Task::class)->make()
+            );
+        });
     }
 }
