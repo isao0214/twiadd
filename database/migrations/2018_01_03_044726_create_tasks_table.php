5a1e6d26603b1a67d279c4fb66878d726c76a341
isao_e <isao0214@gmail.com>
2018-01-03 14:14:54 +0900

    migrations/create_tasks_table.phpを修正

--- database/migrations/2018_01_03_044726_create_tasks_table.php
+++ database/migrations/2018_01_03_044726_create_tasks_table.php
@@ -6,24 +6,17 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateTasksTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('name')->nullable(false);
+            $table->boolean('is_done')->default(false);
+
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::dropIfExists('tasks');
